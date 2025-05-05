da79bb096b8fe3a47b01f26cb77d5a062f8d2cae
quangtiss <quangpro0166@gmail.com>
2025-05-05 17:29:15 +0700

    Add GetEnsureRoot for layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,106 +1,109 @@
 import { Tabs } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { View, Image } from 'react-native';
 
 export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: '#A01850',
-          borderTopRightRadius: 25,
-          borderTopLeftRadius: 25,
-          position: 'absolute', // cần để tạo hiệu ứng bo cong
-          overflow: 'hidden',   // rất quan trọng để bo góc có hiệu lực
-        },
-        tabBarActiveTintColor: '#FDFDFD',
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Học tập',
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={
-                focused
-                  ? require('../../assets/images/navigation/hoctap1.png')
-                  : require('../../assets/images/navigation/hoctap.png')
-              }
-            />
-          ),
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: '#A01850',
+            borderTopRightRadius: 25,
+            borderTopLeftRadius: 25,
+            position: 'absolute', // cần để tạo hiệu ứng bo cong
+            overflow: 'hidden',   // rất quan trọng để bo góc có hiệu lực
+          },
+          tabBarActiveTintColor: '#FDFDFD',
         }}
-      />
+      >
         <Tabs.Screen
-            name='study'
-            options={{
-                href:null,
-                tabBarStyle:{
-                    display:'none'
+          name="index"
+          options={{
+            title: 'Học tập',
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require('../../assets/images/navigation/hoctap1.png')
+                    : require('../../assets/images/navigation/hoctap.png')
                 }
-            }}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='study'
+          options={{
+            href: null,
+            tabBarStyle: {
+              display: 'none'
+            }
+          }}
         >
         </Tabs.Screen>
-      <Tabs.Screen
-        name="productivity"
-        options={{
-          title: 'Năng suất',
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={
-                focused
-                  ? require('../../assets/images/navigation/nangsuat1.png')
-                  : require('../../assets/images/navigation/nangsuat.png')
-              }
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="addcard"
-        options={{
-          title: 'Tạo thẻ',
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={
-                focused
-                  ? require('../../assets/images/navigation/taothe1.png')
-                  : require('../../assets/images/navigation/taothe.png')
-              }
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="notification"
-        options={{
-          title: 'Thông báo',
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={
-                focused
-                  ? require('../../assets/images/navigation/thongbao1.png')
-                  : require('../../assets/images/navigation/thongbao.png')
-              }
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="other"
-        options={{
-          title: 'Khác',
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={
-                focused
-                  ? require('../../assets/images/navigation/khac1.png')
-                  : require('../../assets/images/navigation/khac.png')
-              }
-            />
-          ),
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="productivity"
+          options={{
+            title: 'Năng suất',
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require('../../assets/images/navigation/nangsuat1.png')
+                    : require('../../assets/images/navigation/nangsuat.png')
+                }
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="addcard"
+          options={{
+            title: 'Tạo thẻ',
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require('../../assets/images/navigation/taothe1.png')
+                    : require('../../assets/images/navigation/taothe.png')
+                }
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="notification"
+          options={{
+            title: 'Thông báo',
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require('../../assets/images/navigation/thongbao1.png')
+                    : require('../../assets/images/navigation/thongbao.png')
+                }
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="other"
+          options={{
+            title: 'Khác',
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require('../../assets/images/navigation/khac1.png')
+                    : require('../../assets/images/navigation/khac.png')
+                }
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </GestureHandlerRootView>
   );
 }
--- app/(tabs)/addcard.tsx
+++ app/(tabs)/addcard.tsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import { useRouter } from 'expo-router';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import SuccessModal from '@/components/SuccessModel';
+import SuccessModal from "@/components/SuccessModel";
+import { ScrollView } from "react-native-gesture-handler";
 
 const LoginScreen = () => {
   const router = useRouter();
@@ -12,9 +20,13 @@ const LoginScreen = () => {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title0}>Card</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container} style={{ backgroundColor: "#FFFF", }}>
+      <Image
+        source={require("@/assets/images/logo.png")}
+        style={{ width: 128, height: 128, marginBottom: -20, marginTop: 10 }}
+      />
+      <Text style={styles.title0}>Hello</Text>
+    </ScrollView>
   );
 };
 
@@ -22,79 +34,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 15,
-    backgroundColor: '#fff',
-    justifyContent: 'flex-start',
+    backgroundColor: "#FFFF",
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
   title0: {
-    fontSize: 24,
-    fontFamily: 'Semibold',
-    textAlign: 'center',
-    marginTop: 50,
-    lineHeight: 32,
-  },
-  title1: {
-    fontSize: 16,
-    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#000",
     marginBottom: 20,
-    lineHeight: 24,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  inputIcon: {
-    zIndex: 1,
-    position: 'absolute',
-    marginLeft: 30,
-    top: 18,
-  },
-  hiddenIcon: {
-    zIndex: 1,
-    position: 'absolute',
-    right: 30,
-    top: 18,
-  },
-  input: {
-    flex: 1,
-    height: 53,
-    width: '100%',
-    paddingLeft: 75,
-    paddingRight: 75,
-    backgroundColor: '#E5E5E5',
-    borderRadius: 5,
-    marginBottom: 30,
-    fontSize: 16,
-  },
-  forgotPassword: {
-    color: '#404040',
-    textAlign: 'right',
-    marginBottom: 50,
-    fontWeight: 'bold',
-  },
-  button: {
-    width: '55%',
-    backgroundColor: '#DB2777',
-    padding: 15,
-    borderRadius: 5,
-    alignItems: 'center',
-    alignSelf: 'center',
-    marginBottom: 20,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  register: {
-    color: '#404040',
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  footer: {
-    flexDirection: 'row',
-    position: 'absolute',
-    bottom: 30,
-    alignSelf: 'center',
   },
 });
 
