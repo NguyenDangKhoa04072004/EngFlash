20d07c929ae5b16c5e9722a02796d117cb1565f0
quangtiss <quangpro0166@gmail.com>
2025-05-06 09:37:43 +0700

    Update fea: create card normal

--- app/(tabs)/addcard.tsx
+++ app/(tabs)/addcard.tsx
@@ -19,9 +19,9 @@ const AddCard = () => {
 
   const [showModal, setShowModal] = useState(false);
   const data = [
-    { label: 'Danh từ', value: 'noun' },
-    { label: 'Động từ', value: 'verb' },
-    { label: 'Tính từ', value: 'adj' },
+    { label: "Danh từ", value: "noun" },
+    { label: "Động từ", value: "verb" },
+    { label: "Tính từ", value: "adj" },
   ];
 
   const [wordType, setWordType] = useState(null);
@@ -63,6 +63,83 @@ const AddCard = () => {
           onChange={(item) => setWordType(item.value)}
         />
       </View>
+
+      <View style={styles.AI}>
+        <Image
+          source={require("@/assets/images/navigation/magic-AI-generation.png")}
+        />
+        <Text
+          style={{
+            color: "#A3A3A3",
+            fontFamily: "Regular",
+            fontSize: 16,
+            lineHeight: 24,
+          }}
+        >
+          Magic AI Generation
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        style={{
+          borderRadius: 9999,
+          backgroundColor: "#DB2777",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          height: 40,
+          marginBottom: 15,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "Regular",
+            fontSize: 16,
+            lineHeight: 24,
+            color: "#FDFDFD",
+          }}
+        >
+          Lưu
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "row",
+          height: 70,
+          paddingHorizontal: 15,
+          backgroundColor: "#FFFFFF", // cần thiết để thấy bóng
+          borderBottomLeftRadius: 8,
+          borderTopLeftRadius: 8,
+
+          // iOS shadow
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 4 },
+          shadowOpacity: 0.25,
+          shadowRadius: 4,
+          // Android shadow
+          elevation: 4,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "Bold",
+            fontSize: 16,
+            lineHeight: 24,
+            color: "#1D4ED8",
+            flex: 1,
+          }}
+        >
+          Tạo từ video ngắn
+        </Text>
+        <Image
+          source={require("@/assets/images/navigation/create-from-video.png")}
+          style={{ width: 28, height: 21, marginLeft: "auto" }}
+        />
+      </TouchableOpacity>
     </ScrollView>
   );
 };
@@ -93,7 +170,7 @@ const styles = StyleSheet.create({
     color: "#344054",
   },
   input: {
-    height: 50,
+    height: 45,
     borderWidth: 1,
     borderColor: "#D0D5DD",
     borderRadius: 8,
@@ -105,10 +182,16 @@ const styles = StyleSheet.create({
   dropdown: {
     marginTop: 8,
     height: 50,
-    borderColor: '#D0D5DD',
+    borderColor: "#D0D5DD",
     borderWidth: 1,
     borderRadius: 8,
     paddingHorizontal: 16,
+  },
+  AI: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
   },
 });
 
