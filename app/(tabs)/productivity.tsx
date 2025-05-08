aef74109ce76155445d429bc3c0c160bf6acc876
quangtiss <quangpro0166@gmail.com>
2025-05-08 10:46:14 +0700

    Add chart

--- app/(tabs)/productivity.tsx
+++ app/(tabs)/productivity.tsx
@@ -1,18 +1,41 @@
 import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 //Import thư viện biểu đồ
-import { BarChart, LineChart } from "react-native-gifted-charts";
-
+import { BarChart, LineChart, PieChart, PieChartPro } from "react-native-gifted-charts";
 
 // Khai báo dữ liệu
-const data = [
+const dataBarchart1 = [
   { value: 45, label: "Week 1", frontColor: "#FFD601" },
   { value: 39, label: "Week 2", frontColor: "#FFD601" },
   { value: 61, label: "Week 3", frontColor: "#FFD601" },
 ];
 
-const screenWidth = Dimensions.get("window").width; //Lấy chiều dài màn hình
+const dataLinechart = [
+  { value: 6, label: "Mon" },
+  { value: 11, label: "Tue" },
+  { value: 8, label: "Wed" },
+  { value: 12, label: "Thu" },
+  { value: 18, label: "Fri" },
+  { value: 19, label: "Sat" },
+  { value: 16, label: "Sun" },
+];
 
+const dataBarchart2 = [
+  { value: 4, label: "Last 4", frontColor: "#2563EB" },
+  { value: 8, label: "Last 3", frontColor: "#93C5FD" },
+  { value: 12, label: "Last 2", frontColor: "#FACC15" },
+  { value: 16, label: "Last 1", frontColor: "#F87171" },
+  { value: 6, label: "Today", frontColor: "#DC2626" },
+];
+
+const dataPiechart = [
+  { value: 10, label: "Mon" },
+  { value: 20, label: "Tue" },
+  { value: 30, label: "Wed" },
+  { value: 40, label: "Thu" },
+];
+
+const screenWidth = Dimensions.get("window").width; //Lấy chiều dài màn hình
 
 export default function Productivity() {
   return (
@@ -44,20 +67,19 @@ export default function Productivity() {
         />
       </View>
 
-
       {/* Sơ đồ 1 */}
       <View style={styles.chartContainer}>
         <BarChart
           width={220}
           height={170}
           xAxisThickness={1}
-          xAxisLabelTextStyle={{ fontSize: 14, width: 50, textAlign: 'center' }}
+          xAxisLabelTextStyle={{ fontSize: 14, width: 50, textAlign: "center" }}
           rotateLabel
           barWidth={12}
           spacing={60}
           noOfSections={3}
           barBorderRadius={10}
-          data={data}
+          data={dataBarchart1}
         />
       </View>
 
@@ -80,17 +102,45 @@ export default function Productivity() {
         </View>
       </View>
 
-      <View style={styles.chartContainer}></View>
+      <View style={styles.chartContainer}>
+        <LineChart
+          data={dataLinechart}
+          width={265}
+          spacing={40}
+          xAxisLabelTextStyle={{ fontSize: 14, fontFamily: 'Semibold', textAlign: "center" }}
+          rotateLabel
+          noOfSections={4}
+          height={180}
+        />
+      </View>
       <Text style={styles.label}>Tuần vừa qua</Text>
 
-
-      <View style={styles.chartContainer}></View>
+      <View style={styles.chartContainer}>
+        <PieChart data={dataPiechart} donut />
+      </View>
       <Text style={styles.label}>Thống kê theo bộ từ</Text>
 
-
-      <View style={styles.chartContainer}></View>
+      <View style={styles.chartContainer}>
+        <BarChart
+          height={180}
+          barBorderTopLeftRadius={8}
+          barBorderTopRightRadius={8}
+          noOfSections={9}
+          data={dataBarchart2}
+          isAnimated
+          xAxisLabelTextStyle={{
+            color: '#828282', // màu xám cho nhãn trục x
+            fontSize: 10,
+          }}
+          yAxisTextStyle={{
+            color: '#828282', // màu xám cho nhãn trục y
+            fontSize: 10,
+          }}
+          yAxisColor="#828282" // màu xám cho đường trục Y
+          xAxisColor="#828282" // màu xám cho đường trục X
+        />
+      </View>
       <Text style={styles.label}>5 ngày gần nhất</Text>
-
     </ScrollView>
   );
 }
@@ -114,10 +164,11 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 250,
     marginBottom: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingBottom: 10,
-    overflow: 'hidden'
+    paddingTop: 10,
+    overflow: "hidden",
   },
   cellContainer: {
     flexDirection: "row",
